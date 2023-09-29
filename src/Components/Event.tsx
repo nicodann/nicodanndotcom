3dd34da75ae674beed36445e625c9d73faf0fea3
Nico Dann <nicodann@gmail.com>
2023-09-29 09:47:07 -0400

    Install sass package.

--- src/Components/Event.tsx
+++ src/Components/Event.tsx
@@ -7,15 +7,18 @@ type event = {
     timeZone: string;
   }
   summary: string;
+  location:string;
 }
 
 export default function Event({event}:{event:event}) {
+  console.log("event:", event)
   return (
     <div id="event" key={event.id}>
       <p>{new Date(event.start.dateTime).toDateString()}</p>
       <p>
         {event.summary}
       </p>
+      <p>{event.location}</p>
     </div>
   )
 }
