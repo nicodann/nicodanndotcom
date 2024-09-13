658f2b5c72068b3a7af9d15488444f284a996300
Nico Dann <nicodann@gmail.com>
2024-09-12 21:57:04 -0400

    Bring in bio.md bio_plus branch.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,18 +1,22 @@
 import '../App.scss';
+import Bio from '../Components/Bio';
 import Calendar from "../Components/Calendar";
 
 
 export default function Home() {
   return (
-    <div
-        className="App"
-      >
-        <header id='main-app-heading'>
-          <div className="relatively-positioned-container">
-            <h1>Nico Dann drums</h1>
-          </div>
-        </header>
-        <Calendar />
-      </div> 
+    <div id="app_wrap">
+      <div
+          id="left_side"
+        >
+          <header id='main-app-heading'>
+            <div className="relatively-positioned-container">
+              <h1>Nico Dann drums</h1>
+            </div>
+          </header>
+          <Calendar />
+        </div> 
+        <Bio />
+    </div>
   )
 }
